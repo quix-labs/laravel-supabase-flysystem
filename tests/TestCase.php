f6d49533ce69bdcb49f7bf6e3e1c244c3e046de3
Colant Alan <alan.colant@outlook.fr>
2023-12-21 21:22:22 +0100

    fix ci

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,12 +2,7 @@
 
 namespace Alancolant\FlysystemSupabaseAdapter\Tests;
 
-<<<<<<< HEAD
-use Alancolant\FlysystemSupabaseAdapter\FlysystemSupabaseAdapterServiceProvider;
-=======
 use Alancolant\FlysystemSupabaseAdapter\ServiceProvider;
->>>>>>> cb5e866 (fix ci)
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
